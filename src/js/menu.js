8433625f5a6470440fc8b23dc5044cb19afc215c
Vlad Vorobei <vorobei.official@gmail.com>
2024-05-08 17:45:42 +0300

    Added backdrop for the mobile menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,5 +1,6 @@
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
+  const mobileMenuBackdrop = document.querySelector('.js-menu-backdrop');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
@@ -8,21 +9,17 @@
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
+    mobileMenuBackdrop.classList.toggle('is-open');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    document.body.style.overflow = !isMenuOpen ? 'hidden' : 'auto';
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
